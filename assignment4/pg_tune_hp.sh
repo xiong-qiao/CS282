df34c97cc9f719e289370d99e9550cd7dd2a85be
Xiong Qiao <xiongqiao@berkeley.edu>
2019-11-17 17:59:43 -0500

    add assignment4

---
+++ assignment4/pg_tune_hp.sh
@@ -0,0 +1,16 @@
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 1024 -lr 0.0001 --exp_name pend_b_1024_lr_0.0001
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 1024 -lr 0.001 --exp_name pend_b_1024_lr0.081
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 1024 -lr 0.005 --exp_name pend_b_1024_lr_0.005
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 1024 -lr 0.01 --exp_name pend_b_1024_lr_0.01
+
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 2048 -lr 0.0001 --exp_name pend_b_2048_lr_0.0001
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 2048 -lr 0.001 --exp_name pend_b_2048_lr_0.001
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 2048 -lr 0.005 --exp_name pend_b_2048_lr_0.005
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 2048 -lr 0.01 --exp_name pend_b_2048_lr_0.01
+
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 4096 -lr 0.0001 --exp_name pend_b_4096_lr_0.0001
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 4096 -lr 0.001 --exp_name pend_b_4096_lr_0.001
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 4096 -lr 0.005 --exp_name pend_b_4096_lr_0.005
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 4096 -lr 0.01 --exp_name pend_b_4096_lr_0.01
+
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 7500 -lr 0.001 --exp_name pend_b_7500_lr_0.001
